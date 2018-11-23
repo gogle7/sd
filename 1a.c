04f03fadb14e4b702e0ebadfb11c1b7cca0eed48
gogle7 <40571061+gogle7@users.noreply.github.com>
2018-11-23 21:55:43 +0530

    Update 1a.c

--- 1a.c
+++ 1a.c
@@ -1,29 +1,27 @@
-//1.a.Program to find the sum of 'n' numbers allocate the memory dynamically to the numbers.\n\n");
 #include<stdio.h>
-#include<malloc.h>/*Header file for DMA*/
+#include<malloc.h>
 
 int main()
 {
     int i,n;
     float sum=0,*p;
-    printf("\n\n1.a.Program to find the sum of 'n' numbers allocate the memory dynamically to the numbers.\n\n");
     printf("Enter the number of items.\n");
     scanf("%d",&n);
-    p=(float*)malloc(n*sizeof(int));/*Dynamic Memory Allocation*/
-    if(p==NULL)/*Checking whether pointer is NULL or not (only for negative.)*/
+    p=(float*)malloc(n*sizeof(int));
+    if(p==NULL)
     {
         printf("Allocation Failed.\n");
         return 0;
     }
     printf("Enter the numbers to add.\n");
-    for(i=0;i<n;i++)/*for getting numbers */
+    for(i=0;i<n;i++)
     {
         scanf("%f",(p+i));
     }
-     for(i=0;i<n;i++)/*for sum*/
+     for(i=0;i<n;i++)
     {
         sum += *(p+i);
     }
     printf("The sum of numbers is %f\n\n",sum);
-    free(p);/*Pointer free*/
+    free(p);
 }
