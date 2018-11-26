f20322b6a0cbe95faf56778022ab81ac0b786947
gogle7 <40571061+gogle7@users.noreply.github.com>
2018-11-26 21:55:17 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,69 +1,64 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 100
-struct item
-{
-		int ele;
+#define maxsize 5
+struct item{
+	int ele;
 };
-struct cqueue
-{
-		struct item it[SIZE];
-		int front;
-		int rear;
+struct cqueue{
+	struct item it[maxsize];
+	int front;
+	int rear;
 };
-void insert(struct cqueue *pq,int x);
-int del(struct cqueue *pq);
+void insertitem(struct cqueue *pq,int x);
+int deleteitem(struct cqueue *pq);
 void main()
 {
-		struct cqueue cq;
-		int x,choice,r;
-		cq.front=SIZE-1,cq.rear=SIZE-1;
-		do
+	struct cqueue cq;
+	int x;
+	int choice;
+	int r;
+	cq.front=maxsize-1,cq.rear=maxsize-1;
+	do{
+		printf("\n1.insert\t2:remove\n");
+		printf("enter your choice\n");
+		scanf("%d",&choice);
+		switch(choice)
 		{
-				printf("......MENU.....\n");
-				printf("Enter your choice\n1.INSERT\n2.REMOVE\n3.QUIT\n");
-				scanf("%d",&choice);
-				switch(choice)
-				{
-						case 1:printf("Enter the item to be inserted\n");
-							scanf("%d",&x);
-							insert(&cq,x);
-							break;
-					    case 2:if(cq.front==cq.rear)
-					       		printf("Queue underflow\n");
-					       		else
-					       		{
-					       		r=del(&cq);
-					       		printf("The item deleted is %d\n",r);
-					       		}
-					       		break;
-					    case 3:printf("Quitting operation queue\n");
-					           break;
-					    default:printf("Invalid choice\n");
-					           break;
-					           }
-		}while(choice!=3);
+			case 1:printf("enter the item to be inserted:");
+			scanf("%d",&x);
+			insertitem(&cq,x);
+			break;
+			case 2:if(cq.front==cq.rear)
+			printf("queue underflow\n");
+			else
+			{
+				r=deleteitem(&cq);
+				printf("the item deleted is %d\n",r);
+			}
+			break;
+			default:printf("no such options\n");
 		}
-		void insert(struct cqueue *pq,int x)
-		{
-		if(pq->rear==(SIZE-1))
-		  pq->rear=0;
-		else
-		  (pq->rear)++;
-		 if(pq->front==pq->rear)
-		 {
-		 	printf("Circular queue overflow\n");
-			 exit(1);
-		 }
-		 pq->it[pq->rear].ele=x;
-		 return;
-	 }
-	 int del(struct cqueue *pq)
-	 {
-     if(pq->front==(SIZE-1))
+	}while(choice!=3);
+}
+void insertitem(struct cqueue *pq,int x)
+{
+	if(pq->rear==maxsize-1)
+	pq->rear=0;
+else
+	(pq->rear)++;
+if(pq->rear==pq->front)
+{
+	printf("circular queue overflow\n");
+		exit(1);
+}
+pq->it[pq->rear].ele=x;
+}
+
+int deleteitem(struct cqueue *pq)
+{
+	if(pq->front==maxsize-1)
 		pq->front=0;
 	else
-	 (pq->front)++;
-	 return (pq->it[pq->front].ele);
-	 }
-
+		(pq->front)++;
+	return(pq->it[pq->front].ele);
+}
