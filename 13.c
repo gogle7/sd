890a18825fae8846504c2d2e2d5776ec46dd312e
gogle7 <40571061+gogle7@users.noreply.github.com>
2018-11-27 20:18:32 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,88 +1,104 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct stack{
-
-int item;
-struct stack *link;
-};
-
-typedef struct stack stk;
-stk *top=NULL; 
-//here top can be represented as a head 
-
-void insert();
-int del();
-void show();
-
-
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+struct stack
 {
+int data;
+struct stack *next;
+};
+typedef struct stack NODEPTR;
+NODEPTR *top = NULL;
+NODEPTR *push(NODEPTR *, int);
+NODEPTR *display(NODEPTR *);
+NODEPTR *pop(NODEPTR *);
 
-    int ch=0;
-    printf("1.Insert a New item\n2.Delete a item \n3.Display \n4.Exit\n");
-    while(ch!=4)
+int main() {
+int val;
+int option;
+printf("\n *****MAIN MENU*****");
+
+printf("\n 1. PUSH");
+
+printf("\n 2. POP");
+printf("\n 3. DISPLAY");
+
+printf("\n 4. EXIT");
+do
+{
+printf("\n Enter your option: ");
+		scanf("%d", &option);
+		switch(option)
+		{
+		case 1:
+
+printf("\n Enter the number to be pushed on stack: ");
+			scanf("%d", &val);
+
+top = push(top, val);
+			break;
+		case 2:
+			top = pop(top);
+			break;
+		if (val != -1)
+printf("\n The value at the top of stack is: %d", val);
+else
+printf("\n STACK IS EMPTY");
+	break;
+case 3:
+		top = display(top);
+			break;
+		}
+}while(option != 4);
+return 0;
+}
+NODEPTR *push(NODEPTR *top, int val)
+{
+    NODEPTR *ptr;
+    ptr = (NODEPTR*)malloc(sizeof(NODEPTR));
+    ptr -> data = val;
+    if(top == NULL)
     {
-        printf("\nEnter the choice.\n");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1: insert();
-            break;
-            case 2:del();
-            break;
-            case 3:show();
-            break;
-            case 4:printf("END.\n");
-                    exit(0);
-            break;
-            default:printf("Please Enter the correct choice.\n");
-        }
+
+    ptr -> next = NULL;
+
+    top = ptr;
+    }
+    else
+    {
+
+    ptr -> next = top;
+
+    top = ptr;
+    }
+return top;
+}
+NODEPTR *display(NODEPTR *top)
+{
+    NODEPTR *ptr;
+    ptr = top;
+    if(top == NULL)
+        printf("\n STACK IS EMPTY");
+    else
+    {
+    while(ptr != NULL)
+    {
+        printf("\n %d", ptr -> data);
+        ptr = ptr -> next;
     }
 }
-
-void insert(){
-    stk *new;
-    int ele;
-    new=(stk *)malloc(sizeof(stk));\
-    if(new==NULL){
-    	printf("The allocation failed\n");
-    	exit(0);
-    }
-    printf("Enter the element\n");
-    scanf("%d",&ele);
-    new->item=ele;
-    new->link=NULL;
-    if(top=NULL){
-
-        top=new;
-    }
-    else{
-        new->link=top;
-        top=new;
-    }
+return top;
 }
-
-int del(){
-stk *tem;
-if(top==NULL){
-    printf("Stack uderflow \n");
-    return 0;
-}
-tem=top;
-top=top->link;
-free(tem);
-}
-
-void show(){
-	if(top==NULL){
-	 printf("stack underflow\n");
-	 exit(0);
-	}
-	stk *tem;
-	tem=top;
-	while(tem!=NULL){
-		printf("The item is %d \n",tem->item);
-		tem=tem->link;
-	}
+NODEPTR *pop(NODEPTR *top)
+{
+    NODEPTR *ptr;
+    ptr = top;
+    if(top == NULL)
+    printf("\n STACK UNDERFLOW");
+    else
+    {
+        top = top -> next;
+        printf("\n The value being deleted is: %d", ptr -> data);
+            free(ptr);
+    }
+    return top;
 }
